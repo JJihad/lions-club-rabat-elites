4fbdd2a07cb1b2d5620bbaa9fd431fb891cf9edd
JJihad <jbr.jihad@gmail.com>
2022-04-10 20:02:47 +0000

    use css file instead of module css file

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,46 +1,37 @@
-import React from 'react';
-import { FaBars, FaTimes } from 'react-icons/fa';
-import classes from './MainNavigation.module.css';
-import Logo from '../images/logo-lions-rabat-elites.jpg';
-import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import './mainNavigation.css';
+import Logo from "../images/logo-lions-rabat-elites.jpg";
+import { Link } from "react-router-dom";
 
 function MainNavigation() {
-    const [toggleVisible, setToggleVisible] = useState(false);
-    const handleToggle = () => setToggleVisible(!toggleVisible);
-
-    function navMenu() {
-        return toggleVisible ? 'navMenu activeToggle' : classes.navMenu;
-    }
-
-    // for closed menu : use bars for toggle icon 
-    // for opened menu : use X for toggle icon
-    function togglerIcon() {
-        return (
-            toggleVisible ? <FaTimes size={30} /> : <FaBars size={30} />
-        );
-    }
+  const [toggleActive, setToggleActive] = useState(false);
+  const handleToggle = () => setToggleActive(!toggleActive);
 
   return (
-      <header>
-        <nav className={classes.navbar}>
-          <div className={classes.logo}>
-              <Link to='/'><img src={Logo} alt='logo'/></Link>
-              </div>
-          <ul className={navMenu()}>
-              <li className={classes.navItem}></li>
-              <li className={classes.navItem}><Link to='/'>ABOUT US</Link></li>
-              <li className={classes.navItem}><Link to='/'>CONTACT US</Link></li>
-              <li className={classes.navItem}><Link to='/donate'>DONATE</Link></li>
-          </ul>
+    <div className='navbar'>
+      <div className='logo'>
+        <Link to="/">
+          <img src={Logo} alt="logo" />
+        </Link>
+      </div>
+      <ul className={toggleActive ? 'navMenu active' : 'navMenu'}>
+        <li className='navItem'>
+          <Link to="/">ABOUT US</Link>
+        </li>
+        <li className='navItem'>
+          <Link to="/">CONTACT US</Link>
+        </li>
+        <li className='navItem'>
+          <Link to="/donate">DONATE</Link>
+        </li>
+      </ul>
 
-          <div className={classes.toggler} onClick={handleToggle}>
-            {togglerIcon()}
-          </div>
-      </nav>
-      </header>
-      
-  )
+      <div className='toggler' onClick={handleToggle}>
+      {toggleActive ? (<FaTimes size={30} />) : (<FaBars size={30} />)}
+      </div>
+    </div>
+  );
 }
 
 export default MainNavigation;
